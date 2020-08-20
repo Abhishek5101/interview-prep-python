ff6506470dd97ef84e11bb39960f5951b51ddc54
Abhishek5101 <miabhishekkulkarni@gmail.com>
2020-08-20 14:10:14 +0530

    insertion sort added

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,9 @@
-def bubbleSort(array):
+def bubble_sort(array):
 	for _ in range(len(array)):
 		for i in range(len(array)-1):
 			if array[i] > array[i+1]:
 				array[i], array[i+1] = array[i+1], array[i]
 	return array
+
+
+print(bubble_sort([2, 2, 1, 0, 2, 1, 0]))
