67e787d8e180157473fcedc3032f3909c0fb76d0
Abhishek5101 <miabhishekkulkarni@gmail.com>
2020-09-06 21:43:43 +0530

    swap numbers added

--- swap_integers.py
+++ swap_integers.py
@@ -1,16 +1,36 @@
-def swap_integers(number):
-	ans = ''
+# def swap_integers(number):
+# 	ans = ''
+# 	str_num = str(number)
+# 	popped = ''
+# 	if len(str_num) % 2 != 0:
+# 		popped = str_num[-1]
+# 		str_num = str_num[:len(str_num)-1]
+# 	for i in range(0, len(str_num), 2):
+# 		ans += str_num[i+1]
+# 		ans += str_num[i]
+# 	if popped:
+# 		ans += popped
+# 	return ans
+#
+#
+# print(swap_integers(1234567))
+
+
+def swap_ints(number):
+	ans = ""
+	odd = False
 	str_num = str(number)
-	popped = ''
 	if len(str_num) % 2 != 0:
-		popped = str_num[-1]
-		str_num = str_num[:len(str_num)-1]
-	for i in range(0, len(str_num), 2):
+		last = int(str_num[-1])
+		odd = True
+		number = number - last
+		number = number // 10
+	for i in range(0, len(str_num)-1, 2):
 		ans += str_num[i+1]
 		ans += str_num[i]
-	if popped:
-		ans += popped
-	return ans
+	if odd:
+		ans += str(last)
+	return print(ans)
 
 
-print(swap_integers(1234567))
+swap_ints(123456789)
