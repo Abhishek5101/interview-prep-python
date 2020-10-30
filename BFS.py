dce5312bb9d07e6ae90d743187de58d02ae20cd9
Abhishek5101 <miabhishekkulkarni@gmail.com>
2020-10-30 19:22:58 +0530

    BFS and sum of depths in a binary tree

---
+++ BFS.py
@@ -0,0 +1,19 @@
+# Time complexity O(V+E) for each v, we also visit E (Children)
+# Space Complexity O(V) if the graph is skewed in worst case
+class Node:
+	def __init__(self, name):
+		self.children = []
+		self.name = name
+
+	def addChild(self, name):
+		self.children.append(Node(name))
+		return self
+
+	def breadthFirstSearch(self, array):
+		queue = [self]
+		while len(queue) > 0:
+			current = queue.pop(0)
+			array.append(current.name)
+			for child in current.children:
+				queue.append(child)
+		return array
